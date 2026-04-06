36287e26940466b9f4a3ebd6f254101bc0f921e7
matheusdonasv <matheusdonasv@gmail.com>
2026-04-06 17:38:53 -0300

    Update agnello.js
    Adicionado coleta de dados dos vihos

--- agnello.js
+++ agnello.js
@@ -2,3 +2,6 @@ let nome = prompt("Digite seu nome:");
 
 console.log(`Cadastro realizado. Usuario: ${nome}`)
 alert(`Cadastro realizado! Veja os detalhes no console.`)
+let vinho = prompt("Digite o nome do vinho ");
+let tipo = prompt("Tipo do vinho é tinto, Branco ou Rose ?");
+let safra = prompt("Qual ano é a safra do vinho?");
