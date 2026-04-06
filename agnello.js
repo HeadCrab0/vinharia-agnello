dfbb9d392d9a71d8368c2ebe0c0ab2a261673127
José Henrique <josehenrique.olira@gmail.com>
2026-04-06 17:59:37 -0300

    Adicionando 'quantidade'

--- agnello.js
+++ agnello.js
@@ -1,11 +1,14 @@
 let nome = prompt("Digite seu nome:");
 
-console.log(`Cadastro realizado. Usuario: ${nome}`)
-alert(`Cadastro realizado! Bem-vindo, ${nome}`)
+console.log(`Cadastro realizado. Usuario: ${nome}`);
+alert(`Cadastro realizado! Bem-vindo, ${nome}`);
 let vinho = prompt("Digite o nome do vinho ");
-console.log(`nome do vinho: ${vinho}`)
+console.log(`nome do vinho: ${vinho}`);
 let tipo = prompt("Tipo do vinho é Tinto, Branco ou Rose ?");
-console.log(`tipo do vinho: ${tipo}`)
-let safra = prompt("Qual ano é a safra do vinho?");
-console.log(`safra do vinho: ${safra}`)
-alert("Cadastro realizado, veja as infomações no console.")
+console.log(`tipo do vinho: ${tipo}`);
+let safra = prompt("Qual ano é a safra do vinho:");
+console.log(`safra do vinho: ${safra}`);
+let quantidade = prompt("Quantos desse vinho estão em estoque:");
+console.log(`quantidade em estoque do vinho ${nome} é: ${quantidade});
+alert("Cadastro realizado, veja as infomações no console.");
+
