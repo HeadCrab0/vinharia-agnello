dae456c578515dce2741a6eba30ce041b32bff49
Enzo Cassiano <enzocm786@gmail.com>
2026-04-06 17:46:45 -0300

    Update agnello.js

--- agnello.js
+++ agnello.js
@@ -3,5 +3,9 @@ let nome = prompt("Digite seu nome:");
 console.log(`Cadastro realizado. Usuario: ${nome}`)
 alert(`Cadastro realizado! Veja os detalhes no console.`)
 let vinho = prompt("Digite o nome do vinho ");
+console.log(`nome do vinho: ${vinho}`)
 let tipo = prompt("Tipo do vinho é tinto, Branco ou Rose ?");
+console.log(`tipo do vinho: ${tipo}`)
 let safra = prompt("Qual ano é a safra do vinho?");
+console.log(`safra do vinho: ${safra}`)
+alert("cadastro realizdo, veja infomações no console.")
