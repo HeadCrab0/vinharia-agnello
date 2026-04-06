44a7998ebf41ecef8c46e7ef6a19589367b24a60
José Henrique <josehenrique.olira@gmail.com>
2026-04-06 17:56:17 -0300

    Formatando o agnello.js
    corrigindo algumas palavras

--- agnello.js
+++ agnello.js
@@ -1,11 +1,11 @@
 let nome = prompt("Digite seu nome:");
 
 console.log(`Cadastro realizado. Usuario: ${nome}`)
-alert(`Cadastro realizado! Veja os detalhes no console.`)
+alert(`Cadastro realizado! Bem-vindo, ${nome}`)
 let vinho = prompt("Digite o nome do vinho ");
 console.log(`nome do vinho: ${vinho}`)
-let tipo = prompt("Tipo do vinho é tinto, Branco ou Rose ?");
+let tipo = prompt("Tipo do vinho é Tinto, Branco ou Rose ?");
 console.log(`tipo do vinho: ${tipo}`)
 let safra = prompt("Qual ano é a safra do vinho?");
 console.log(`safra do vinho: ${safra}`)
-alert("cadastro realizdo, veja infomações no console.")
+alert("Cadastro realizado, veja as infomações no console.")
